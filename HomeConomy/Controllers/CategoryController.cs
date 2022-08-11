dfc41b2d5f4ce6349f530cb5c2a70ae3abfa74cb
shan15dev <shansai@bluewin.ch>
2022-08-11 08:50:02 +0200

    first controller method implemented

--- HomeConomy/Controllers/CategoryController.cs
+++ HomeConomy/Controllers/CategoryController.cs
@@ -1,6 +1,24 @@
+using HomeConomy.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace HomeConomy.Controllers;
 
-public class CategoryController
+[ApiController]
+[Route("api/Categories")]
+public class CategoryController : ControllerBase
 {
+    private readonly DataContext _dataContext;
     
+    public CategoryController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<CategoryModel>>> GetAllCategories()
+    {
+        return Ok(await _dataContext.Categories.ToListAsync());
+    }
+
 }
