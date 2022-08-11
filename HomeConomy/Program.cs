b20a6095e7dfe2acedd0f5b723ccec6456472a7e
shan15dev <shansai@bluewin.ch>
2022-08-11 09:43:02 +0200

    Connection attributes added into Program.cs

--- HomeConomy/Program.cs
+++ HomeConomy/Program.cs
@@ -1,11 +1,28 @@
+using HomeConomy;
+using HomeConomy.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.Configure<AppConfig>(builder.Configuration);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options => options
+    .UseMySql(configuration["ConnectionString"], new MySqlServerVersion(new Version(8, 0, 29)))
+    .LogTo(Console.WriteLine, LogLevel.Information)
+    .EnableSensitiveDataLogging()
+    .EnableDetailedErrors()
+);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
