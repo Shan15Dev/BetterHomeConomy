bb265ddd4124468bfd5ef1f20ae693943c2bc438
shan15dev <shansai@bluewin.ch>
2022-08-11 08:45:25 +0200

    DataContextedited

--- HomeConomy/Model/DataContext.cs
+++ HomeConomy/Model/DataContext.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace HomeConomy.Model;
 
-public class DataContext
+public class DataContext : DbContext
 {
-    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<CategoryModel> Categories { get; set; }
+    public DbSet<ExpenseModel> Expenses { get; set; }
+    public DbSet<RoleModel> Roles { get; set; }
+    public DbSet<UserModel> Users { get; set; }
 }
